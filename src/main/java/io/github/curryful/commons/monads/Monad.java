230225736b22321f5b32e3308a81eff139995490
Nico Lerchl <nico@lerchl.one>
2024-05-06 17:41:37 +0200

    orElseGet method for Monads

--- src/main/java/io/github/curryful/commons/monads/Monad.java
+++ src/main/java/io/github/curryful/commons/monads/Monad.java
@@ -1,5 +1,7 @@
 package io.github.curryful.commons.monads;
 
+import java.util.function.Supplier;
+
 public abstract class Monad<T> {
 
     protected T value;
@@ -18,6 +20,16 @@ public abstract class Monad<T> {
 	 */
 	public T orElse(T other) {
 		return hasValue() ? value : other;
+	}
+
+	/**
+	 * If a value is present, returns the value, otherwise returns the value produced by the supplier.
+	 * 
+	 * @param supplier the supplier of the value to be returned if there is no value present
+	 * @return the value, if present, otherwise the value produced by the supplier
+	 */
+	public T orElseGet(Supplier<? extends T> supplier) {
+		return hasValue() ? value : supplier.get();
 	}
 
     public boolean hasValue() {
